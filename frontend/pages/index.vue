3aceec6d03e8ff7267be673b70b417ff23fe84d3
Dina Bader <dina19bader@gmail.com>
2024-07-06 11:10:16 +0300

    Frontend <> disabeled submit button on login and register until form is filled

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,79 +1,85 @@
 <template>
   <div class="background">
-      <v-container fluid>
-        <v-row justify="center">
-          <v-col cols="12" sm="8" md="6" lg="4">
-              <p class="header-title">Invoice Managment System</p>
-            <div class="form-container">
-              <p class="form-title">Login</p>
-              <v-form ref="loginForm" @submit.prevent="handleLogin">
-                <v-text-field
-                  v-model="username"
-                  :rules="userNameRules"
-                  label="Username"
-                ></v-text-field>
-                <v-text-field
-                  v-model="password"
-                  label="Password"
-                  :rules="passwordRules"
-                  type="password"
-                ></v-text-field>
-                <v-alert v-if="loginError" type="error">{{ loginError }}</v-alert>
-                <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-              </v-form>     
-              <NuxtLink to="/register">
-                <v-btn class="register-button" >Register </v-btn>
-              </NuxtLink>  
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-  </template>
-  
-  <script>
-  import {loginUser} from '../service/loginService'
-  export default {
-    data() {
-      return {
-        username: '',
-        password: '',
-        userNameRules:[
-          value => { 
-            if(value) return true
-            return "You must enter a Username"
-          }
-        ],
-        passwordRules:[
-          value => { 
-            if(value) return true
-            return "You must enter a Password"
-          }
-        ],
-        loginError: ''
-      };
-    },
-    methods: {
-      async handleLogin() {
-        const isValid = this.$refs.loginForm.validate();
-        if (!isValid) {
-          return;
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <p class="header-title">Invoice Management System</p>
+          <div class="form-container">
+            <p class="form-title">Login</p>
+            <v-form ref="loginForm" @submit.prevent="handleLogin">
+              <v-text-field
+                v-model="username"
+                :rules="userNameRules"
+                label="Username"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                :rules="passwordRules"
+                type="password"
+              ></v-text-field>
+              <v-alert v-if="loginError" type="error">{{ loginError }}</v-alert>
+              <v-btn class="mt-2" :disabled="submitButtonDisabled" type="submit" block>Submit</v-btn>
+            </v-form>
+            <NuxtLink to="/register">
+              <v-btn class="register-button">Register</v-btn>
+            </NuxtLink>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { loginUser } from '../service/loginService';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      userNameRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a Username';
         }
-        try {
-          const accessToken = await loginUser(this.username, this.password);
-          console.log('Access Token:', accessToken); 
-          localStorage.setItem('accessToken',accessToken);
-          this.$router.push({ path: '/dashboard' });
-        } catch (error) {
-          console.error('Error Logging in:', error);
-          this.loginError = 'Invalid username or password. Please try again.';
+      ],
+      passwordRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a Password';
         }
+      ],
+      loginError: ''
+    };
+  },
+  computed: {
+    submitButtonDisabled() {
+      return !this.username || !this.password;
+    }
+  },
+  methods: {
+    async handleLogin() {
+      const isValid = await this.$refs.loginForm.validate();
+      if (!isValid) {
+        return;
+      }
+
+      try {
+        const accessToken = await loginUser(this.username, this.password);
+        console.log('Access Token:', accessToken); 
+        localStorage.setItem('accessToken', accessToken);
+        this.$router.push({ path: '/dashboard' });
+      } catch (error) {
+        console.error('Error Logging in:', error);
+        this.loginError = 'Invalid username or password. Please try again.';
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  @import '@/style/login.css' ;
-  </style>
-  
+  }
+};
+</script>
+
+<style scoped>
+@import '@/style/login.css';
+</style>
--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,83 +1,91 @@
 <template>
-    <div class="background">
-        <v-container fluid>
-            <v-row justify="center">
-                <v-col cols="12" sm="8" md="6" lg="4">
-                    <p class="header-title">Invoice Managment System</p>
-                    <v-form ref="registerForm" @submit.prevent="handleRegister">
-                        <div class="form-container">
-                            <p class="form-title">Register</p>
-                            <v-text-field v-model="userName" label="Username" :rules="usernameRules"></v-text-field>
-                            <v-text-field v-model="firstName" label="Firstname" :rules="firstnameRules"></v-text-field>
-                            <v-text-field v-model="lastName" label="Lastname" :rules="lastnameRules"></v-text-field>
-                            <v-text-field v-model="password" label="Password " :rules="passwordRules" type="password"></v-text-field>
-                            <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                            <NuxtLink to="/">
-                            <v-btn class="register-button" >Login </v-btn>
-                            </NuxtLink>  
-                        </div>
-                    </v-form>
-                </v-col>
-            </v-row>
-        </v-container>
-    </div>
-  </template>
-  
-  <script>
-  import {RegisterUser} from '@/service/registerService'
-  export default {
-    data() {
-      return {
-        firstName: '',
-        lastName: '',
-        userName: '',
-        password: '',
-        usernameRules:[
-          value=>{
-              if(value) return true
-              return "You must enter a Username";
-          }
-        ],
-        firstnameRules:[
-          value=>{
-              if(value) return true
-              return "You must enter a First Name";
-          }
-        ],
-        lastnameRules:[
-          value=>{
-              if(value) return true
-              return "You must enter a Last Name";
-          }
-        ],
-        passwordRules:[
-          value=>{
-              if(value) return true
-              return "You must enter a Password";
-          }
-        ],
-      };
-    },
-    methods:{
-        async handleRegister(){
-            const isValid = this.$refs.registerForm.validate();
-            if (!isValid) {
-            console.log('Form is not valid');
-            return;
-            }
-            try{
-                const accessToken = await RegisterUser(this.firstName, this.password,this.lastName,this.userName);
-                localStorage.setItem('accessToken',accessToken);
-                this.$router.push({ path: '/dashboard' });
-            }catch(error){
-                console.error("Error signing in: ",error);
-            }
+  <div class="background">
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <p class="header-title">Invoice Management System</p>
+          <v-form ref="registerForm" @submit.prevent="handleRegister">
+            <div class="form-container">
+              <p class="form-title">Register</p>
+              <v-text-field v-model="userName" label="Username" :rules="usernameRules"></v-text-field>
+              <v-text-field v-model="firstName" label="Firstname" :rules="firstnameRules"></v-text-field>
+              <v-text-field v-model="lastName" label="Lastname" :rules="lastnameRules"></v-text-field>
+              <v-text-field v-model="password" label="Password" :rules="passwordRules" type="password"></v-text-field>
+              
+              <v-btn class="mt-2" :disabled="!isFormValid" type="submit" block>Submit</v-btn>
+              
+              <NuxtLink to="/">
+                <v-btn class="register-button">Login</v-btn>
+              </NuxtLink>
+              
+            </div>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { RegisterUser } from '@/service/registerService';
+
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      password: '',
+      usernameRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a Username';
         }
+      ],
+      firstnameRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a First Name';
+        }
+      ],
+      lastnameRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a Last Name';
+        }
+      ],
+      passwordRules: [
+        value => {
+          if (value) return true;
+          return 'You must enter a Password';
+        }
+      ],
+    };
+  },
+  computed: {
+    isFormValid() {
+      return this.userName && this.firstName && this.lastName && this.password;
     }
-  };
-  </script>
-  
-  <style scoped>
-  @import '@/style/login.css'
-  </style>
-  
+  },
+  methods: {
+    async handleRegister() {
+      const isValid = await this.$refs.registerForm.validate();
+      if (!isValid) {
+        console.log('Form is not valid');
+        return;
+      }
+      try {
+        const accessToken = await RegisterUser(this.firstName, this.password, this.lastName, this.userName);
+        localStorage.setItem('accessToken', accessToken);
+        this.$router.push({ path: '/dashboard' });
+      } catch (error) {
+        console.error("Error signing in: ", error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import '@/style/login.css';
+</style>
